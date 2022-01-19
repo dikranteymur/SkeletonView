f8c0820e19009219efd302f3924348e78c8f0458
Dikran Teymur <dikran.teymur@appcent.mobi>
2022-01-19 16:50:26 +0300

    add new files

---
+++ Pods/Target Support Files/Pods-SkeletonViewPOC/Pods-SkeletonViewPOC-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SkeletonViewPOC : NSObject
+@end
+@implementation PodsDummy_Pods_SkeletonViewPOC
+@end
---
+++ SkeletonViewPOC/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SkeletonViewPOC
+//
+//  Created by Dikran Teymur on 19.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
